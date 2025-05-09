6339ea8ed12d764e38f9c01067b365d7430d44a5
Lidoan <lidoanpinto@gmail.com>
2025-05-09 17:04:19 -0300

    add spring security classes

---
+++ src/main/java/com/lido/user/infrastructure/repository/PhoneRepository.java
@@ -0,0 +1,9 @@
+package com.lido.user.infrastructure.repository;
+
+import com.lido.learningspring.infrastructure.entity.Phone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhoneRepository extends JpaRepository<Phone, Long> {
+}
---
+++ src/main/java/com/lido/user/infrastructure/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.lido.user.infrastructure.repository;
+
+import com.lido.user.infrastructure.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
