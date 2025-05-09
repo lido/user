b40709b66057650316adb28cf3b50a2afa848fca
Lidoan <lidoanpinto@gmail.com>
2025-05-09 16:30:38 -0300

    Initial commit

---
+++ src/test/java/com/lido/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lido.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
