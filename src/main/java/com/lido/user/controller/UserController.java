5c8c9b828e08f639380f891fc425835057a463e7
Lidoan <lidoanpinto@gmail.com>
2025-05-11 12:11:34 -0300

    add method get, delete and login

--- src/main/java/com/lido/user/controller/UserController.java
+++ src/main/java/com/lido/user/controller/UserController.java
@@ -2,12 +2,14 @@ package com.lido.user.controller;
 
 import com.lido.user.business.UserService;
 import com.lido.user.business.dto.UserDTO;
+import com.lido.user.infrastructure.entity.User;
+import com.lido.user.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -15,9 +17,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
    @PostMapping
     public ResponseEntity<UserDTO> saveUser(@RequestBody UserDTO userDTO){
         return ResponseEntity.ok(userService.saveUser(userDTO));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UserDTO userDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userDTO.getEmail(),
+                        userDTO.getPassword()
+                )
+        );
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<User> findUserByEmail(@RequestParam("email") String email){
+        return ResponseEntity.ok(userService.findUserByEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deleteUserByEmail(@PathVariable("email") String email){
+        userService.deleteUserByEmail(email);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/lido/user/infrastructure/repository/AddressRepository.java
+++ src/main/java/com/lido/user/infrastructure/repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package com.lido.user.infrastructure.repository;
 
-import com.lido.learningspring.infrastructure.entity.Address;
+
+import com.lido.user.infrastructure.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
