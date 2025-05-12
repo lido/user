62b4ef303fe395477f7b7f97890aa192f6d3d12c
Lidoan <lidoanpinto@gmail.com>
2025-05-12 10:24:53 -0300

    added methods to record user address and phone number

--- src/main/java/com/lido/user/business/AddressService.java
+++ src/main/java/com/lido/user/business/AddressService.java
@@ -3,8 +3,11 @@ package com.lido.user.business;
 import com.lido.user.business.converter.AddressConverter;
 import com.lido.user.business.dto.AddressDTO;
 import com.lido.user.infrastructure.entity.Address;
+import com.lido.user.infrastructure.entity.User;
 import com.lido.user.infrastructure.exceptions.ResourceNotFoundException;
 import com.lido.user.infrastructure.repository.AddressRepository;
+import com.lido.user.infrastructure.repository.UserRepository;
+import com.lido.user.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,8 @@ public class AddressService {
 
     private final AddressRepository addressRepository;
     private final AddressConverter addressConverter;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
     public AddressDTO updateAddress(Long idAddress, AddressDTO addressDTO){
 
@@ -24,6 +29,21 @@ public class AddressService {
         Address address =  addressConverter.upadateAdress(addressDTO, entity);
 
         return addressConverter.toAdressDTO(addressRepository.save(address));
+    }
+
+    public AddressDTO registerAddress(String token, AddressDTO addressDTO){
+
+        String email = jwtUtil.extractEmailname(token.substring(7));
+
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email not found" + email )
+        );
+
+        Address address = addressConverter.toAddressEntity(user.getId(), addressDTO);
+
+        return addressConverter.toAdressDTO(
+                addressRepository.save(address)
+        );
     }
 
 }
--- src/main/java/com/lido/user/business/converter/UserConverter.java
+++ src/main/java/com/lido/user/business/converter/UserConverter.java
@@ -1,54 +1,26 @@
 package com.lido.user.business.converter;
 
-import com.lido.user.business.dto.AddressDTO;
-import com.lido.user.business.dto.PhoneDTO;
 import com.lido.user.business.dto.UserDTO;
-import com.lido.user.infrastructure.entity.Address;
-import com.lido.user.infrastructure.entity.Phone;
 import com.lido.user.infrastructure.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @Component
 public class UserConverter {
+
+    private final AddressConverter addressConverter;
+    public final PhoneConverter phoneConverter;
 
     public User toUser(UserDTO userDTO){
         return User.builder()
                 .name(userDTO.getName())
                 .email(userDTO.getEmail())
                 .password(userDTO.getPassword())
-                .addresses(toAdressList(userDTO.getAddresses()))
-                .phones(toPhonesList(userDTO.getPhones()))
+                .addresses(addressConverter.toAdressList(userDTO.getAddresses()))
+                .phones(phoneConverter.toPhonesList(userDTO.getPhones()))
                 .build();
     }
-
-    public List<Address> toAdressList(List<AddressDTO> addressDTOS){
-        return addressDTOS.stream().map(this::toAdress).toList();
-    }
-
-    public Address toAdress(AddressDTO addressDTO){
-        return Address.builder()
-                .cep(addressDTO.getCep())
-                .city(addressDTO.getCity())
-                .state(addressDTO.getState())
-                .complement(addressDTO.getComplement())
-                .number(addressDTO.getNumber())
-                .street(addressDTO.getStreet())
-                .build();
-    }
-
-    public List<Phone> toPhonesList(List<PhoneDTO> phoneDTOS){
-        return phoneDTOS.stream().map(this::toPhone).toList();
-    }
-
-    public Phone toPhone(PhoneDTO phoneDTO){
-        return Phone.builder()
-                .ddd(phoneDTO.getDdd())
-                .number(phoneDTO.getNumber())
-                .build();
-    }
-
 
     public UserDTO toUserDTO(User user){
         return UserDTO.builder()
@@ -56,36 +28,8 @@ public class UserConverter {
                 .name(user.getName())
                 .email(user.getEmail())
                 .password(user.getPassword())
-                .addresses(toAdressListDTO(user.getAddresses()))
-                .phones(toPhonesListDTO(user.getPhones()))
-                .build();
-    }
-
-    public List<AddressDTO> toAdressListDTO(List<Address> addresses){
-        return addresses.stream().map(this::toAdressDTO).toList();
-    }
-
-    public AddressDTO toAdressDTO(Address address){
-        return AddressDTO.builder()
-                .id(address.getId())
-                .cep(address.getCep())
-                .city(address.getCity())
-                .state(address.getState())
-                .complement(address.getComplement())
-                .number(address.getNumber())
-                .street(address.getStreet())
-                .build();
-    }
-
-    public List<PhoneDTO> toPhonesListDTO(List<Phone> phones){
-        return phones.stream().map(this::toPhoneDTO).toList();
-    }
-
-    public PhoneDTO toPhoneDTO(Phone phone){
-        return PhoneDTO.builder()
-                .id(phone.getId())
-                .ddd(phone.getDdd())
-                .number(phone.getNumber())
+                .addresses(addressConverter.toAdressListDTO(user.getAddresses()))
+                .phones(phoneConverter.toPhonesListDTO(user.getPhones()))
                 .build();
     }
 
